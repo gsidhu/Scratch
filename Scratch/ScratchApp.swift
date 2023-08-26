59de57f9a54dddc80878bafa3364a8d0367b931b
Gurjot Sidhu <stuff@thatgurjot.com>
2023-08-26 12:02:41 +0530

    Initial Commit

---
+++ Scratch/ScratchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScratchApp.swift
+//  Scratch
+//
+//  Created by Gurjot Sidhu on 26/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ScratchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
